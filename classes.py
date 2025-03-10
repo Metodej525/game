5fd38be1927bcd0decc3745b1030034e5fc97abd
Metodej525 <metodej.alexa@gmail.com>
2025-03-10 22:05:44 +0100

    Hehe

---
+++ classes.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import List, Optional,Dict,Any
+
+@dataclass
+class MoveItemSetup:
+    source: List[Dict[str, Any]]
+    target: List[Dict[str, Any]]
+    item: Dict[str, Any]
+class MoveItem:
+    def __init__(self,setup: MoveItemSetup):
+        self.source = setup.source
+        self.target = setup.target
+        self.item = setup.item
+    def move(self):
+        if self.item in self.source:
+            item_index = self.source.index(self.item)
+            item_move = self.source[item_index].copy()
+            self.source.pop(item_index)
+            self.target.append(item_move)
+            return True
+        return False
